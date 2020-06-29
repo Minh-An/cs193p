4457e234a1eab15590ebbd20b5ad170eba3f95e5
An Doan <minh.an.doan@gmail.com>
2020-06-28 21:53:45 -0700

    Lecture 1: play with Views.

--- Memorize/Memorize/ContentView.swift
+++ Memorize/Memorize/ContentView.swift
@@ -10,8 +10,36 @@ import SwiftUI
 
 struct ContentView: View {
     var body: some View {
-        Text("Hello, World!")
+      return VStack() {
+        ForEach(0..<4) { _ in
+          CardView(isFlipped: true)
+        }
+      }
+      .padding()
+      .foregroundColor(Color.orange)
+      .font(Font.largeTitle)
     }
+}
+
+
+struct CardView: View {
+  var isFlipped: Bool
+  
+  var body: some View {
+    return ZStack() {
+      if (isFlipped)
+      {
+        RoundedRectangle(cornerRadius: 10.0).fill(Color.white)
+        RoundedRectangle(cornerRadius: 10.0).stroke(lineWidth: 5.0)
+        Text("❤️")
+      }
+      else {
+        RoundedRectangle(cornerRadius: 10.0).fill()
+      }
+    }
+  }
+  
+  
 }
 
 struct ContentView_Previews: PreviewProvider {
