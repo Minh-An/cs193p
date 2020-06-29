f3cb2b635db39a303f0915f3259be98fd242d8b7
An Doan <minh.an.doan@gmail.com>
2020-06-28 21:40:05 -0700

    Initialize starter project for lecture 1.

---
+++ Memorize/Memorize/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  Memorize
+//
+//  Created by An Doan on 6/28/20.
+//  Copyright © 2020 minhandoan. All rights reserved.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, World!")
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
